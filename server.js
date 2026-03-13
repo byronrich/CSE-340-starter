01b0b1910fb6ceaed6fdfeaba3300d1c7eb72cc8
byronrich <alex.richr@gmail.com>
2026-03-13 14:16:49 -0600

    Fix home route and views path

--- server.js
+++ server.js
@@ -1,44 +1,40 @@
 /* ******************************************
- * This server.js file is the primary file of the 
- * application. It is used to control the project.
- *******************************************/
-/* ***********************
  * Require Statements
- *************************/
-const express = require("express")
-const env = require("dotenv").config()
-const app = express()
-const expressLayouts = require("express-ejs-layouts")
-const static = require("./routes/static")
-
+ *******************************************/
+const express = require("express");
+const env = require("dotenv").config();
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const static = require("./routes/static");
 
 /* ******************************************
  * View Engine and Templates
- * ******************************************/
-app.set("view engine", "ejs")
-app.use(expressLayouts)
-app.set("layout", "./layouts/layout") // not at views root
+ *******************************************/
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(expressLayouts);
+app.set("layout", "./layouts/layout");
 
-/* ***********************
+/* ******************************************
+ * Static Files
+ *******************************************/
+app.use(express.static("public"));
+
+/* ******************************************
  * Routes
- *************************/
-
-app.use(static)
-
-/* ***********************
- * Local Server Information
- * Values from .env (environment) file
- *************************/
-const port = process.env.PORT
-const host = process.env.HOST
-
-/* ***********************
- * Log statement to confirm server operation
- *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+ *******************************************/
 app.get("/", (req, res) => {
   res.render("layouts/index");
 });
 
+app.use(static);
+
+/* ******************************************
+ * Server
+ *******************************************/
+const port = process.env.PORT;
+const host = process.env.HOST;
+
+app.listen(port, () => {
+  console.log(`app listening on ${host}:${port}`);
+});
