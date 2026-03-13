8b00d4b5279a5c0fbbeac479369fab9f0a05c636
byronrich <alex.richr@gmail.com>
2026-03-12 22:29:24 -0600

    Forgot to update my code

--- server.js
+++ server.js
@@ -8,11 +8,21 @@
 const express = require("express")
 const env = require("dotenv").config()
 const app = express()
+const expressLayouts = require("express-ejs-layouts")
 const static = require("./routes/static")
+
+
+/* ******************************************
+ * View Engine and Templates
+ * ******************************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "./layouts/layout") // not at views root
 
 /* ***********************
  * Routes
  *************************/
+
 app.use(static)
 
 /* ***********************
@@ -28,3 +38,6 @@ const host = process.env.HOST
 app.listen(port, () => {
   console.log(`app listening on ${host}:${port}`)
 })
+app.get('/', (req, res) => {
+  res.render('index');
+});
