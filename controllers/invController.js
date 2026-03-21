4c7337678bdac3c115cb8c7e5784c3956805af35
byronrich <alex.richr@gmail.com>
2026-03-21 00:34:01 -0600

    week 3 assignment fixes

--- controllers/invController.js
+++ controllers/invController.js
@@ -9,23 +9,43 @@ const invCont = {}
 invCont.buildByClassificationId = async function (req, res, next) {
   const classification_id = req.params.classificationId
   const data = await invModel.getInventoryByClassificationId(classification_id)
-  const grid = await utilities.buildClassificationGrid(data)
   let nav = await utilities.getNav()
+
+  // SAFETY CHECK — prevents crashes
+  if (!data || data.length === 0) {
+    return next({
+      status: 404,
+      message: "No vehicles found for this classification."
+    })
+  }
+
+  const grid = await utilities.buildClassificationGrid(data)
   const className = data[0].classification_name
+
   res.render("./inventory/classification", {
     title: className + " vehicles",
     nav,
     grid,
   })
 }
+
 /* ***************************
  *  Build vehicle detail view
  * ************************** */
 invCont.buildByInvId = async function (req, res, next) {
   const invId = req.params.invId
   const data = await invModel.getInventoryByInvId(invId)
-  const detail = await utilities.buildDetailView(data[0])
   let nav = await utilities.getNav()
+
+  // SAFETY CHECK — prevents crashes
+  if (!data || data.length === 0) {
+    return next({
+      status: 404,
+      message: "Vehicle not found."
+    })
+  }
+
+  const detail = await utilities.buildDetailView(data[0])
 
   res.render("./inventory/detail", {
     title: `${data[0].inv_make} ${data[0].inv_model}`,
--- utilities/index.js
+++ utilities/index.js
@@ -1,4 +1,3 @@
-const utilities = require("./utilities/")
 const invModel = require("../models/inventory-model")
 const Util = {}
 
@@ -25,47 +24,47 @@ Util.getNav = async function (req, res, next) {
   return list
 }
 
-module.exports = Util
-
 /* **************************************
 * Build the classification view HTML
 * ************************************ */
-Util.buildClassificationGrid = async function(data){
+Util.buildClassificationGrid = async function (data) {
   let grid
-  if(data.length > 0){
+  if (data.length > 0) {
     grid = '<ul id="inv-display">'
-    data.forEach(vehicle => { 
+    data.forEach(vehicle => {
       grid += '<li>'
-      grid +=  '<a href="../../inv/detail/'+ vehicle.inv_id 
-      + '" title="View ' + vehicle.inv_make + ' '+ vehicle.inv_model 
-      + 'details"><img src="' + vehicle.inv_thumbnail 
-      +'" alt="Image of '+ vehicle.inv_make + ' ' + vehicle.inv_model 
-      +' on CSE Motors" /></a>'
+      grid += '<a href="/inv/detail/' + vehicle.inv_id 
+      + '" title="View ' + vehicle.inv_make + ' ' + vehicle.inv_model 
+      + ' details"><img src="/images/vehicles/' + vehicle.inv_thumbnail 
+      + '" alt="Image of ' + vehicle.inv_make + ' ' + vehicle.inv_model 
+      + ' on CSE Motors" /></a>'
       grid += '<div class="namePrice">'
       grid += '<hr />'
       grid += '<h2>'
-      grid += '<a href="../../inv/detail/' + vehicle.inv_id +'" title="View ' 
+      grid += '<a href="/inv/detail/' + vehicle.inv_id + '" title="View ' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + ' details">' 
       + vehicle.inv_make + ' ' + vehicle.inv_model + '</a>'
       grid += '</h2>'
       grid += '<span>$' 
-      + new Intl.NumberFormat('en-US').format(vehicle.inv_price) + '</span>'
+      + new Intl.NumberFormat("en-US").format(vehicle.inv_price) + '</span>'
       grid += '</div>'
       grid += '</li>'
     })
     grid += '</ul>'
-  } else { 
-    grid += '<p class="notice">Sorry, no matching vehicles could be found.</p>'
+  } else {
+    grid = '<p class="notice">Sorry, no matching vehicles could be found.</p>'
   }
   return grid
 }
+
 /* ****************************************
  * Middleware For Handling Errors
  * Wrap other function in this for 
  * General Error Handling
  **************************************** */
 Util.handleErrors = fn => (req, res, next) =>
-   Promise.resolve(fn(req, res, next)).catch(next)
+  Promise.resolve(fn(req, res, next)).catch(next)
+
 /* **************************************
 * Build the vehicle detail HTML
 * ************************************ */
@@ -73,7 +72,8 @@ Util.buildDetailView = async function (vehicle) {
   let detail = `
     <section id="vehicle-detail">
       <div class="detail-image">
-        <img src="${vehicle.inv_image}" alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
+        <img src="/images/vehicles/${vehicle.inv_image}" 
+             alt="Image of ${vehicle.inv_make} ${vehicle.inv_model}">
       </div>
 
       <div class="detail-info">
@@ -88,3 +88,5 @@ Util.buildDetailView = async function (vehicle) {
   `
   return detail
 }
+
+module.exports = Util
