2a1e9ee232eaf3bff3cc592d89eed5a471847383
byronrich <alex.richr@gmail.com>
2026-03-25 03:07:37 -0600

    fixes to get database connection working

--- database/index.js
+++ database/index.js
@@ -1,23 +1,14 @@
 const { Pool } = require("pg")
 require("dotenv").config()
 
-/* ***************
- * Connection Pool
- * SSL Object needed for local testing of app
- * But will cause problems in production environment
- * If - else will make determination which to use
- * *************** */
 let pool
-if (process.env.NODE_ENV == "development") {
+
+// Local development (no SSL)
+if (process.env.NODE_ENV === "development") {
   pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-      rejectUnauthorized: false,
-    },
+    connectionString: process.env.DATABASE_URL
   })
 
-  // Added for troubleshooting queries
-  // during development
   module.exports = {
     async query(text, params) {
       try {
@@ -30,9 +21,16 @@ if (process.env.NODE_ENV == "development") {
       }
     },
   }
-} else {
+}
+
+// Render production (SSL required)
+else {
   pool = new Pool({
     connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
   })
+
   module.exports = pool
 }
